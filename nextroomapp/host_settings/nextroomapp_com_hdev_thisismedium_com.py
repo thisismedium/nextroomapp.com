f899fa74a2bca4dd8f6d90c5d4c3863f900c5652
coptix <Ben Weaver weaver@coptix.com>
2010-07-09 15:49:12 -0400

    Hide "Available in App Store" icon.

---
+++ nextroomapp/host_settings/nextroomapp_com_hdev_thisismedium_com.py
@@ -0,0 +1,8 @@
+
+from common import *
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'nextroomapp_com_hdev_thisismedium_com'
+DATABASE_USER = 'nextroomapp_com_hdev_thisismedium_com'
+DATABASE_PASSWORD = SECRET_KEY
+DATABASE_HOST = 'psqlhost'
+
